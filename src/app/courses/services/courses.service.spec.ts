17d0cce7123be5c4845168232509f19748967247
Bryant <bryant.franks@gmail.com>
2021-03-30 13:24:22 -0400

    11&12-httpClientTestingModule implementation

---
+++ src/app/courses/services/courses.service.spec.ts
@@ -0,0 +1,47 @@
+import { TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { CoursesService } from "./courses.service";
+import { COURSES } from "../../../../server/db-data";
+
+describe("CoursesService", () => {
+
+    let coursesService: CoursesService;
+    let httpTestingController: HttpTestingController;
+
+    beforeEach(() => {
+
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                CoursesService
+            ]
+        });
+
+        coursesService = TestBed.get(CoursesService);
+        httpTestingController = TestBed.get(HttpTestingController);
+
+    });
+
+    it('should retrieve all courses', ()=>{
+        // tests observables in findAllCourses() by subscribing
+        coursesService.findAllCourses()
+        .subscribe(courses => {
+            // first test asserts that courses are indeed there
+            expect(courses).toBeTruthy('No Courses Returned');
+            // checks that there are indeed 12 courses available
+            expect(courses.length).toBe(12, 'incorrect number of courses');
+
+            const course = courses.find(course => course.id == 12);
+
+            expect(course.titles.description).toBe('Angular Testing Course');
+        });
+
+        // sets up controller to only make one call
+        const req = httpTestingController.expectOne('/api/courses');
+
+        // tests the call that the method is GET
+        expect(req.request.method).toEqual("GET");
+
+        req.flush({payload: Object.values(COURSES)});
+    });
+});
